b2a06da29faabfcd1ec4ffe1264fdb5ec375faed
MarikKomarik <marksmc1337@gmail.com>
2023-03-05 20:27:28 +0300

    tipical

--- src/stroki.java
+++ src/stroki.java
@@ -1,259 +1,296 @@
 import java.util.Arrays;
-import java.util.Locale;
+import java.util.Collections;
 import java.util.Scanner;
+
 public class stroki {
-
     public static void main(String[] args) {
-        /*
-//1
-          Scanner sc = new Scanner(System.in); // создаём объект класса Scanner121212121
-        String Dopusk=" qwertyuiopasdfghjklzxcvbnm1234567890_ ";//все что можно
-        String digits="0987654321";//без коментариев
-        System.out.print("Введите строку");
-        String str=sc.nextLine();
-        str=str.toLowerCase();//регистр весь маленький
-        int S=0;
-        int chetchikNeravensta=0;
-        int Simno=-10;
-        if (!(str.isEmpty())){//проверка не пустой ли
-            for(int x=0;x<10;x++){//Проверку на цифру
-                if(str.charAt(0)==digits.charAt(x)){
-                    S=-2;
-                    System.out.println("-2 НАЧИНАЕТСЯ С ЦИФРРЫ");
-                    break;
+
+        //2
+        /* System.out.println("Задайте размер массива");
+        Scanner input=new Scanner(System.in);
+        int N= input.nextInt();
+        int[] array =new int[N];
+        int min=0;
+        int max=0;
+        int sum=0;
+        for (int x=array.length-1;x>=0;x--){
+            array[x]= (int) (Math.random()*100%10);
+        }
+
+       for (int x=array.length-1;x>=0;x--){
+           if (array[x]>max){
+             max=array[x];
+           }
+           if (array[x]<min){
+               min=array[x];
+           }
+        }
+        System.out.print("Все нечетные значения:");
+
+        for (int x=array.length-1;x>=0;x--){
+            sum=sum+array[x];
+            if (array[x]%2==1) {
+                System.out.print(array[x]);
+            }
+        }
+
+        System.out.println("\n"+max+"-Максимум");
+        double arf= (double)sum/N;
+        System.out.println(min+"-минимум"+"\n"+sum+"-сумма всех чисел ");
+        System.out.println(arf+"- Среднее арифметическое");
+
+//3
+        System.out.println("Введите строку");
+        Scanner input=new Scanner(System.in);
+        String str=input.nextLine();
+        int leh=str.length();
+        char n='a';
+        int koln=0;
+        char[] strchar=str.toCharArray();
+        for (int x=0;x<str.length();x++){
+            if(Character.compare(strchar[x],n)==0){
+                koln=koln+1;
+            }
+        }
+        double atf=(double) 100/str.length()*koln;
+        System.out.println(atf+"- Процент вхождения символа 'а' в строке");
+
+//4
+        int N=5;
+        int[][] arr=new int[N][N];
+        int sums=0,prst=1,maxd=0;
+        for (int x=0;x<N;x++){
+            for (int y=0;y<N;y++) {
+                arr[x][y] = (int) (Math.random() * 100 % 10);
+            }
+        }
+        System.out.println("Ниже сама таблица для визуализации");
+        for (int x=0;x<N;x++){
+            for (int y=0;y<N;y++) {
+                System.out.print(arr[x][y]+" ");
+            }
+            System.out.println();
+        }
+        for (int x=0;x<N;x++){
+            for (int y=0;y<N;y++) {
+                sums=sums+arr[x][y];
+            }
+            System.out.println(sums+" сумма "+(x+1)+" строки");
+            sums=0;
+        }
+        for (int y=0;y<N;y++){
+            for (int x=0;x<N;x++) {
+                prst=prst*arr[x][y];
+                if ((x==y)&&(arr[x][y]>maxd)){
+                    maxd=arr[x][y];
                 }
             }
-            for (int x=0;x<str.length();x++){// проверка на символы
-                for (int i=0;i<Dopusk.length()-1;i++){
-                    if(!(str.charAt(x)== Dopusk.charAt(i))){//проверка кажджого символа есть ли он в допуске
-                        chetchikNeravensta=chetchikNeravensta+1;//перемнная сколько раз не равен символу в допуске,
-                    }
-                }
-                if(chetchikNeravensta==Dopusk.length()-1){//если не равен всем символам в допуске
-                    Simno=x;
-                    break;//прекратить цикл если найден недопустимый
-                }
-                chetchikNeravensta=0;//обнуляем счетчик
-            }
-        }else{//если пустая
-            S=-1;
-            System.out.println("-1 , ПУСТАЯ СТРОКА");
+            System.out.println(prst+" произведение чисел "+(y+1)+ " столбца");
+            prst=1;
         }
-        if ((Simno>-1)&& !(S==-1)&&!(S==-2)){//если каждый символ прошел допуск и строка не пустая
-            S=-100;
-            System.out.println("НЕДОПУСТИМЫЕ СИМВОЛЫ, ИНДЕКС ПЕРВОГО - "+Simno);
-        }
-        if(S==0){//если все норм
-            System.out.println(S+", ВСЕ НОРМ");
-        }
+        System.out.println(maxd+" - максимальное число главной диагонали");
 
-
-//2
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Введите N");
-        int N= sc.nextInt();
-        System.out.println("Введите шаблон строки");
-        String shablonS=sc.next();
-        String strN="";
-        int i=N/shablonS.length();//количество целых строк которое поместится
-        int o=N%shablonS.length();//остаток от деления, сколько символов еще надо
-        for (int x=0;x<i;x++){
-            strN=strN.concat(shablonS);//тут и + можно было
-        }
-        for (int x=0;x<o;x++){
-            strN=strN+shablonS.charAt(x);
-        }
-        System.out.println("Строка длины N , заполнена шаблонами- "+strN);
-
-//3 4
-        String S ="Ты ЧеГо ТаКаЯ БяКа)? Ne Zlis))";
-        String dopusk="ЙЦУКЕЁНГШЩЗХЪЭЖДЮБЛЬОТРИПМАСВЧЫФЯ";
-        char[] dopuskch=dopusk.toCharArray();//массив русских букв
-        char[] Sarr=S.toUpperCase().toCharArray();//массив для сравнения без регистра
-        char[] ishod=S.toCharArray();//массив в котором будут меняться русские буквы
-        for (int x=0;x<S.length();x++){//для каждого символа строки
-            for(int i=0;i<dopusk.length();i++){//проверятеся для каждого русского
-                if(Sarr[x]==dopuskch[i]){//если русский то регистр нижний
-                    ishod[x]=Sarr[x];
-                }
+//5
+        System.out.println("Введите строку через пробел");
+        Scanner input=new Scanner(System.in);
+        String str=input.nextLine();
+        char n=' ';
+        int kol=1;
+        char[] strchar=str.toCharArray();
+        for (int x=0;x<str.length();x++){
+            if(strchar[x]==' '){
+                kol=kol+1;
             }
         }
-        System.out.println(S);
-        System.out.println(ishod);
- //5-6
-        String S="amsterdam";
-        char c='m';
-        for(int x=0;x<S.length();x++){
-            if(S.charAt(S.length()-1)==c){//символ по индексу
-                S=S.substring(0,S.length()-1);//метод берет символы строки с 1 индекса
-                break;                     //по последний
-            }
-        }
-        System.out.println(S);
-//7
-        String S="0123456789абвгде";
-        String reverse="";
-        char[] S1=S.toCharArray();
-        int N=10;//скока
-        int K=8;//скакова
-        int i=0;//счетчик для N
-        int kn=K+N;//чтобы менять можно было
-        if (K>S.length()) {
-            System.out.println("Типа пустая строка");
-        }else {
-            if(K+N>S.length()-1){
-                kn=S.length();
-            }
-            for (int x =kn-1; i < N; x--) {//начиная с к+н в обратную сторону
-                reverse = reverse + S1[x];
-                i++;
-            }
+        System.out.println("Вы ввели "+kol+" слов");
+
+//6
 
 
+int[] chi=new int[15] ;
+int clenght=0;
+int nlenght=0;
+        for(int x=0;x< chi.length;x++){//генерация случайного массива
+            chi[x]=(int)(Math.random()*100);
+            System.out.print(chi[x]+" ");
         }
-        System.out.println(S);
-        System.out.println(reverse+" N="+N+" K="+K);
+        System.out.println("- случайный массив с числами от 0 до 99" );
+        for(int x=0;x< chi.length;x++){//проверяем сколько будет весить
+            if(chi[x]%2==1){
+                nlenght=nlenght+1;
+            }
+            else {
+                clenght=clenght+1;
+            }
+        }
+int[] chet=new int[clenght];
+int[] nechet=new int[nlenght];
+        clenght=0;//это чтобы переменные не вводить
+        nlenght=0;
+        for(int x=0;x<chi.length;x++){//инициализация
+            if(chi[x]%2==1){
+                nechet[nlenght]=chi[x];
+                nlenght=nlenght+1;
+            }
+            else {
+                chet[clenght]=chi[x];
+                clenght=clenght+1;
+            }
+        }
+        Arrays.sort(chet);// метод сортировки от меньшего к большему
+        Arrays.sort(nechet);
+        for(int x=0;x<clenght;x++){
+            System.out.print(chet[x]+" ");
+        }
+        System.out.println("-четный массив по возрастанию ");
+        for(int x=0;x<nlenght;x++){
+            System.out.print(nechet[x]+" ");
+        }
+        chet.reverse()
+        System.out.println("- нечетный массив по возрастанию ");
+
 //8
-        String S="Саша Шла Саша по шоссе и сосала сушку";
-        String isk="";
-        String slovo="Вика";
-        int len = S.length();
-        int K=5;
-        int N=10;
-        if (K + N > len) {
-            N = len - K;//чтобы N + K=len
+         /*int[] M =new int[100];
+        int kol=0;
+        for (int x=1;x<M.length;x++){
+          M[x]=(int)(Math.random()*100);//сл. массив
         }
-        if (K > len) {
-            System.out.println(0);
-        }else {
-            isk=S.substring(K,K+N);//где будем искать
+        for (int x=0;x<M.length;x++){
+            if((M[x]>0) && (M[x]<25)){//условие
+                kol=kol+1;
+            }
         }
-        int index =isk.indexOf(slovo);//ищем индекс вхождения Саши
-        System.out.println(S+"  ");
-        if(!(index==-1)) {
-            System.out.print("Индекс вхождения "+slovo+" равен:");
-            System.out.println(index+K+" /K="+K+" N="+N);//прибавляем К, относительно S
-
-        }else{
-            System.out.println(slovo+" не найдено "+0);
-        }
+        System.out.println(kol+" элементов");
 //9
-        String S="_12_45_789012_4_67";//оригинал поиска
-        String slovo="_";//что будем искать
-        int index;//индекс первого вхождения
-        int K=0;//переменная(индекс посл. вхож) которая урезает поиск с начала
-        String isk=S.substring(K);//где будем искать, но в обрезанном виде
-        //метод выше создает строку из оригинала , но начало берет с индекса К
-        for (int x=0;x<S.length();x++){//для каждого символа в строке
-            index=isk.indexOf(slovo);//смотрим индекс вхождения
-            if(index>-1){//если поиск удался
-                K=K+1;//то переменную увеличиваем,до тех пор пока искать будет нечего
-                isk=S.substring(K);//иск урезаем в начале , уже без того что нашли в прошлый раз
-            }//если ничего не нашлось, то индекс последнего вхождения  это K-1
-        }
-        System.out.println(K-1+" - индекс последнего вхождения:");
-        System.out.println(slovo+" в "+S);
-
-         *
-
- //10
-        String S="_12_45_789012_4_67";//оригинал поиска
-        String slovo="_";//что будем искать
-        int index;//индекс первого вхождения
-        int K1=3;//номер вхождения которое надо найти
-        int K0=0;//переменная для счета вхождений
-        int K=0;//переменная(индекс посл. вхож) которая урезает поиск с начала
-        String isk=S.substring(K);//где будем искать, но в обрезанном виде
-        //метод выше создает строку из оригинала , но начало берет с индекса К
-        for (int x=0;x<S.length();x++){//для каждого символа в строке
-            index=isk.indexOf(slovo);//смотрим индекс вхождения
-            if(index>-1){//если поиск удался
-                K0=K0+1;//счетчик вхождений
-                K=K+index+1;//Урезаем больше чем в прошлый раз чтобы отбросить прошлое вхождение
-                isk=S.substring(K);//иск урезаем в начале , уже без того что нашли в прошлый раз
-                if (K0==K1){//если вхождение равно искомому то выводим на экран
-                    System.out.println(K-1+" - индекс "+K0+" вхождения");
-                    System.out.println(slovo+" в "+S);
-                    break;
-                }
+        System.out.println("Введите строку");
+        Scanner input=new Scanner(System.in);
+        String str=input.nextLine();
+        int s=0;//s для проверки палиндром или нет
+        int m;//м для удобства , чтобы текса меньше
+        int y=0;//счетчик спереди
+        int i=0;//счетчик сзади
+        char[] strchar=str.toCharArray();
+        int v=strchar.length-1;//для удобства
+        for (int x=0;x<str.length()/2+1;x++){
+            if (strchar[v-i]==' '){
+                i=i+1;
             }
+            if (strchar[y]==' '){
+                y=y+1;
+            }
+            m=Character.compare(strchar[v-i],strchar[y]);
+            if((m==0)||(m==32)||(m==-32)){//32 потому что если разный регистр то 32 получается
+                s=s+1;
+            }else {
+                s=-1000;
+            }
+            y++;
+            i++;
         }
-        if (K0<K1){
-            System.out.println(0);//условие задачи
-        }
-
-
-//14
-        int dvoich=0;
-        int S=dvoich;//тавтология
-        String shest="";
-        int temp2=0;
-        if(dvoich==0){//если 0 то 0
-            shest="0";
+        if (s>0){
+            System.out.println("Это палиндром");
         }else{
-            for (int x=0;x<10;x++) {
-                temp2 = dvoich % 16;//остаток надо приписывать левее
-                dvoich=dvoich / 16;//а потом остаток от целого деления и так по новой
-                if(temp2<10) {//если меньше 10 то цифру можно не менять
-                    if((temp2==0)&&(dvoich==0)){//если целая часть и остаток = 0 то конец цикла
-                        break;
-                    }else {
-                        shest = temp2 + shest;//остаток приписываем слева
-                    }
-                }else{//если больше 10 то надо уже символ от А до F
-                    temp2=temp2-10;//узнаем индекс
-                    shest=(char)(65+temp2)+shest;//в инете нашел, символ А(65) представляется
-                    // в виде юникода и в зависимости от темп2 юникод меняется
-                    //а потом обратно преобразуется в символ и прибавляется к строке
+            System.out.println("Это не палиндром");
+        }
+
+//10
+        int N=5;
+        int[][] arr=new int[N][N];
+        int sump=0,sumot=1;
+        for (int x=0;x<N;x++){
+            for (int y=0;y<N;y++) {
+                arr[x][y] = (int) ((Math.random() * 200-100) % 10);
+            }
+        }
+        System.out.println("Ниже сама таблица для визуализации");
+        for (int x=0;x<N;x++){
+            for (int y=0;y<N;y++) {
+                System.out.print(arr[x][y]+" ");
+            }
+            System.out.println();
+        }
+        System.out.print("Координаты нулей (столбик)(строка):");
+        for (int x=0;x<N;x++){
+            for (int y=0;y<N;y++) {
+                if (arr[x][y]==0){
+                    System.out.print("("+(y+1)+")("+(x+1)+"),");
+                }
+                if (arr[x][y]<0){
+                    sumot=sumot+1;
+                }else{if (arr[x][y]!=0){
+                    sump=sump+1;
+                }
                 }
             }
         }
-        System.out.println("число "+S+" в 16ричном:"+shest);
+        System.out.println("\n"+sumot+"количество отрицательных чисел");
+        System.out.println(sump+"количество положительных чисел");
 
-//15
-        String _16="4D2";//А в юникоде это 65 , а 0 это 48
-        double temp;//хранит в себе смысл 16ричной цифры в 10чном виде
-        double stepen=0;//без коментариев
-        double dvoich=0;//итог, дабл потому что метод pow только с даблом работает
-        for(int x=_16.length()-1;x>=0;x--){//для каждого символа в строке 16чной
-            if((int)_16.charAt(x)>64){//если это символ, то онимаем от юникода 55
-                temp=(_16.charAt(x)-55)*(Math.pow(16.0,stepen));//цифру умножаем на порядок
-            }else{//если число то 48 от юникода
-                temp=(_16.charAt(x)-48)*(Math.pow(16.0,stepen));
+//11
+        System.out.println("Введите строку на английском");
+        Scanner input=new Scanner(System.in);
+        String str=input.nextLine();
+        int ipo = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);//метод берет символ из строки по индексу
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                    ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+                ipo++;
             }
-            dvoich=dvoich+temp;//прибавляем к общему результату
-            stepen=stepen+1;//с каждым циклом степень 16 надо увеличивать
         }
-        System.out.println(_16+" это "+(int)dvoich);
+        double percentage = (double)ipo / str.length() * 100;
+        System.out.println("Процент вхождения" + percentage + "%");
 
-//12
-         String S="Сегодня хороший день без грусти и зла ";
-         int K=1;//номер слова которое надо найти
-         int chet=0;//для сравнения с условием
-         String isk=S;
-         for (int x=0;x<K;x++){
-             chet++;
-             if(chet==K){//пишем иск до пробела(слово)
-                 System.out.println((isk.substring(0,isk.indexOf(" ")))+"-"+K+" Слово");
-                 System.out.println(S);
-                 break;
-             }
-             isk=isk.substring(isk.indexOf(" ")+1);//иск - предложение со след слова
-         }
 
-  //12       */
-        String S="Сегодня хороший день без грусти и зла ";
-        int W=1;
-        String[] Sarr=new String[W];
-        String isk=S;
-        System.out.println("W="+W+"\n"+S+"\n"+"Элементы массива ниже");
-        for (int x=0;x<W;x++){
-            Sarr[x]=isk.substring(0,isk.indexOf(" "));
-            System.out.print("["+Sarr[x]+"]");
-            isk=isk.substring(isk.indexOf(" ")+1);//иск - предложение со след слова
+
+     //12
+        int[] arr=new int[20];
+        int ipo=0;
+        for (int x=0;x<20;x++){
+            arr[x]=(int)((Math.random()*2000-1000));//случ ч от -1000 до +
+            System.out.print(arr[x]+" ");//для наглядности
         }
+        for (int x=0;x<20;x++){
+           if (arr[x]>=-100 && arr[x]<=100){
+               ipo=ipo+1;
+           }
+        }
+        System.out.println("\n"+ipo+"- количество элементов в диапазоне от -100 до +100");
+
+  //13
+        int[] arr=new int[5];
+        int ipo=0;
+        for (int x=0;x<5;x++){
+            arr[x]=(int)((Math.random()*20-10));//случ ч от -10 до +10
+            System.out.print(arr[x]+" ");//для наглядности
+        }
+        System.out.println("-исходный массив");
+        int[] arryp=new int[5];//делаем 2 массив
+        System.arraycopy(arr,0,arryp,0,5);/*метод копирования массива где
+        откуда, с какого эл копировать , куда, с какого эл втсавлять, сколько вставлять*/
+        /*Arrays.sort(arryp);//сортировка копии
+        for (int x=0;x<5;x++){
+            System.out.print(arryp[x]+" ");
+        }
+        System.out.println("-копия по порядку");
+        for (int x=0;x<5;x++){
+            if (arr[x]==arryp[x]){
+                System.out.println(arr[x]+"-упорядоченное");
+            }else {
+                System.out.println(arr[x]+"-неупорядоченное");
+            }
+        }
+
+
+         */
+
+
+        //14
+
+
+
+
 
 
     }
 }
-
